368959763285cbbae487d488eb8f82c67788c31f
drmodun <jan.modun.st@gmail.com>
2023-02-19 23:57:42 +0100

    fully functional version

--- js.js
+++ js.js
@@ -5,6 +5,7 @@ const menus = document.querySelectorAll(".select-menu")
 const closed = new Event("closed")
 const chosenOptions = [0, 0]
 const button = []
+const invalidInputs = [];
 const optionsDictionary = [{
     0: "Elementary School",
     1: "Middle School",
@@ -15,22 +16,26 @@ const optionsDictionary = [{
     1: "5-10 years",
     2: "10-20 years",
     3: "20+ years"
-}       
+}
 ];
 
 const sumbitButton = document.querySelector(".links  button");
 
 let currentPerson = window.localStorage.getItem("data")
-if (currentPerson!=null){
+if (currentPerson != null) {
     currentPerson = JSON.parse(currentPerson)
     inputs[0].value = currentPerson.firstName;
     inputs[1].value = currentPerson.lastName;
 
     activators[0].children[0].innerHTML = optionsDictionary[0][Number(currentPerson.profession)]
-    activators[1].children[0].innerHtml = optionsDictionary[1][Number(currentPerson.level)]
+    activators[1].children[0].innerHTML = optionsDictionary[1][Number(currentPerson.level)]
+    chosenOptions[0] = Number(currentPerson.profession);
+    chosenOptions[1] = Number(currentPerson.level);
+    menus[0].children[1].children[chosenOptions[0] * 2].style.color = "#1360a0"
+    menus[1].children[1].children[chosenOptions[1] * 2].style.color = "#1360a0"
     console.log(Number(currentPerson.profession))
 }
-else{
+else {
     menus[0].firstChild.value = optionsDictionary[0][0]
     menus[1].firstChild.value = optionsDictionary[1][0]
 }
@@ -42,22 +47,33 @@ const person = {
     level: ""
 }
 inputs.forEach(element => {
-    //element.style.backgroundColor="#4555";
     element.style.border = "1px solid black"
     element.style.borderRadius = "5px"
     element.addEventListener("focus", (event) => {
-        element.style.backgroundColor = "transparent"
-        element.style.outlineColor = "#1360a0";
-        element.parentElement.firstElementChild.style.color = "#1360a0"        //element.style.backgroundColor = "white"
-        focusedInputs.push(element)
-        console.log("sdfsadf", element.parentElement.firstElementChild)
-
-
+        element.style.outline = "none"
+        if (!invalidInputs.includes(element)) {
+            //console.log("wezt 8owes")
+            element.style.backgroundColor = "transparent"
+            element.style.borderColor = "#1360a0";
+            element.parentElement.firstElementChild.style.color = "#1360a0"        //element.style.backgroundColor = "white"
+            focusedInputs.push(element)
+            console.log("sdfsadf", element.parentElement.firstElementChild)
+        }
     });
-    element.addEventListener("blur", (event) => {
+    element.addEventListener("focusout", (event) => {
         console.log("sdfsadf2")
         element.parentElement.firstElementChild.style.color = "black"
-        element.style.outlineColor = "default";
+        if (element.value.trim().length == 0) {
+            element.parentElement.lastElementChild.style.display = "flex";
+            element.style.borderColor = "#b30000"
+            invalidInputs.push(element)
+        }
+        else {
+            element.parentElement.lastElementChild.style.display = "none";
+            element.style.borderColor = "black"
+            if (invalidInputs.includes(element))
+                invalidInputs.splice(invalidInputs.indexOf(element))
+        }
         focusedInputs.splice(focusedInputs.indexOf({ ...element }))
     });
     element.addEventListener("mouseover", (event) => {
@@ -68,9 +84,6 @@ inputs.forEach(element => {
     })
     element.addEventListener("mouseout", (event) => {
         element.style.backgroundColor = "transparent"
-    })
-    element.addEventListener("error", (event) => {
-        return
     })
 })
 menus.forEach(element => {
@@ -94,6 +107,7 @@ menus.forEach(element => {
                 element.children[0].innerHTML = child.innerHTML;
                 chosenOptions[Array.prototype.indexOf.call(menus, element)] = childNodes.indexOf(child) / 2 //divided by two because dividers exist
                 child.style.color = "#1360a0"
+                console.log("aishdhipas")
                 element.dispatchEvent(closed)
             })
             child.addEventListener("mouseover", event => {
@@ -119,22 +133,27 @@ menus.forEach(element => {
 })
 sumbitButton.addEventListener("mouseover", event => {
     sumbitButton.style.cursor = "pointer"
+    sumbitButton.style.backgroundColor = "#1360a0"
 
 })
 sumbitButton.addEventListener("mouseout", event => {
     sumbitButton.style.cursor = "default"
+    sumbitButton.style.backgroundColor = "#00447c"
+
 })
 
 sumbitButton.addEventListener("click", event => {
-    if (inputs[0].value.trim().length > 0)
-        person.firstName = inputs[0].value.trim();
-    else
-        console.log("ne");
-    if (inputs[1].value.trim().length > 0)
-        person.lastName = inputs[1].value.trim();
-    else
-        console.log("ne");
-    person.profession = chosenOptions[0]
-    person.level = chosenOptions[1]
-    window.localStorage.setItem("data", JSON.stringify(person))
+    if (!inputs[0].value.trim().length > 0)
+        inputs[0].dispatchEvent("focusout");
+    else {
+        if (!inputs[1].value.trim().length > 0)
+            inputs[1].dispatchEvent("focusout");
+        else {
+            person.firstName = inputs[0].value.trim();
+            person.lastName = inputs[1].value.trim();
+            person.profession = chosenOptions[0]
+            person.level = chosenOptions[1]
+            window.localStorage.setItem("data", JSON.stringify(person))
+        }
+    }
 })
